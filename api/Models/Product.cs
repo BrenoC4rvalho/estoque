dbcae8f917e596d69f7b8b10354d9b7dcf53173e
Breno Carvalho <103794884+BrenoC4rvalho@users.noreply.github.com>
2025-06-14 23:15:20 -0300

    feat: create product model.

---
+++ api/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+
+namespace api.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Product name is required.")]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Cost price is required.")]
+        [Range(0, double.MaxValue, ErrorMessage = "Cost price must be a positive value.")]
+        public decimal CostPrice { get; set; }
+        
+        [ValidateNever]
+        public ICollection<InventoryItem> IventoryItems { get; set; }
+
+    }
+}
