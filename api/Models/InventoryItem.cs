12c9ccec15773dc83856029c092a9acb92acc24a
Breno Carvalho <103794884+BrenoC4rvalho@users.noreply.github.com>
2025-06-14 23:14:58 -0300

    feat: create inventoryItem model.

---
+++ api/Models/InventoryItem.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+
+namespace api.Models
+{
+    public class InventoryItem
+    {
+
+        public int Id { get; set; }
+
+        [Required]
+        public int StoreId { get; set; }
+
+        [ValidateNever]
+        public Store Store { get; set; }
+
+        [Required]
+        public int ProductId { get; set; }
+
+        [ValidateNever]
+        public Product Product { get; set; }
+
+        [Required]
+        [Range(0, int.MaxValue, ErrorMessage = "Quantity must be zero or a positive number.")]
+        public int Quantity { get; set; }
+
+    }
+}
