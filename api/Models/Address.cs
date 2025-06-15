6f8919eb260ad21bdb8260a561375dd4dc2d6bd1
Breno Carvalho <103794884+BrenoC4rvalho@users.noreply.github.com>
2025-06-14 23:14:33 -0300

    feat: create store model and address.

---
+++ api/Models/Address.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace api.Models
+{
+    public class Address
+    {
+
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Street is required.")]
+        [StringLength(150)]
+        public string Street { get; set; }
+
+        [Required(ErrorMessage = "City is required.")]
+        [StringLength(100)]
+        public string City { get; set; }
+
+        [Required(ErrorMessage = "Neighborhood is required.")]
+        [StringLength(100)]
+        public String Neighborhood { get; set; }
+
+        [Required(ErrorMessage = "Zip code is required.")]
+        [StringLength(9, MinimumLength = 9, ErrorMessage = "The zipcode must be between 2 and 100 characters long.")]
+        public string ZipCode { get; set; }
+
+        [StringLength(100, ErrorMessage = "The complement must not exceed 100 characters.")]
+        public string Complement { get; set; }
+
+    }
+}
---
+++ api/Models/Store.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+
+namespace api.Models
+{
+    public class Store
+    {
+
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Store name is required.")]
+        [StringLength(100, MinimumLength = 2, ErrorMessage = "The store name must be between 2 and 100 characters long.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Store email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email address format.")]
+        [StringLength(100, ErrorMessage = "The email must not exceed 100 characters.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Store password is required.")]
+        [StringLength(255, MinimumLength = 8, ErrorMessage = "The password must have at least 6 characters")]
+        public string Password { get; set; }
+
+        [Required]
+        public int AddressId { get; set; }
+
+        [ValidateNever]
+        public Address Address { get; set; }
+
+        [ValidateNever]
+        public ICollection<InventoryItem> InventoryItems { get; set; } 
+
+    }
+}
